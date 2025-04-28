cc82d64dc1280e036e87339dafbac81a73fc97f0
elo369 <rohitrikam88@gmail.com>
2025-04-28 12:17:05 +0530

    update css

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,11 +7,10 @@ import {
   IconBrandGithub,
   IconBrandLinkedin,
   IconBrandTwitter,
-  IconMail
+  IconMail,
 } from "@tabler/icons-react";
 
 export default function Contract() {
-  
   return (
     <div
       id="contact"
@@ -20,77 +19,90 @@ export default function Contract() {
       {/* Contact Info Section */}
       <section className="flex flex-col items-center md:w-1/2 text-center space-y-6 md:space-y-8">
         <div className="text-3xl md:text-5xl text-white lg:text-6xl font-bold  relative inline-block">
-            Contact <span className="bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-transparent bg-clip-text">Me</span>
-            <motion.div 
-              className="absolute -bottom-2 left-0 right-0 h-1 bg-[#292929] rounded-full"
-              initial={{ width: "0%" }}
-              whileInView={{ width: "100%" }}
-              viewport={{ once: true }}
-              transition={{ duration: 1, delay: 0.5 }}
-            />
-          </div>
+          Contact{" "}
+          <span className="bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-transparent bg-clip-text">
+            Me
+          </span>
+          <motion.div
+            className="absolute -bottom-2 left-0 right-0 h-1 bg-[#292929] rounded-full"
+            initial={{ width: "0%" }}
+            whileInView={{ width: "100%" }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 0.5 }}
+          />
+        </div>
 
-        <div className="flex flex-col space-y-4 w-full max-w-xs">
-        {/* Twitter Button */}
-        <button
-          className="relative group/btn flex space-x-2 items-center justify-start px-4 py-2 rounded-md font-medium bg-zinc-900 text-neutral-300 shadow-lg hover:shadow-xl transition-all"
-          type="button"
-        >
-          <Link href="https://x.com/rohitrikame88" target="_blank" rel="noopener noreferrer" className="flex space-x-2">
-            <IconBrandTwitter className="h-5 w-5" />
-          <span>Twitter</span>
-          </Link>
-          <BottomGradient />
-        </button>
-
-        <button
-          className="relative group/btn flex space-x-2 items-center justify-start px-4 py-2 rounded-md font-medium bg-zinc-900 text-neutral-300 shadow-lg hover:shadow-xl transition-all"
-          type="button"
-        >
-          <Link
-            href="mailto:rohitrikame88@gmail.com" // Replace with your actual email
-            className="flex space-x-2"
+        <div className="flex flex-col space-y-4 w-[90vw] max-w-xs">
+          {/* Twitter Button */}
+          <button
+            className="relative group/btn flex space-x-2 items-center justify-start px-4 py-2 rounded-md font-medium bg-zinc-900 text-neutral-300 shadow-lg hover:shadow-xl transition-all"
+            type="button"
           >
-            {/* You can replace the icon below with any email icon you want */}
-            <IconMail className="h-5 w-5" /> 
-            <span>Email Me</span>
-          </Link>
-          <BottomGradient />
-        </button>
+            <Link
+              href="https://x.com/rohitrikame88"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex space-x-2"
+            >
+              <IconBrandTwitter className="h-5 w-5" />
+              <span>Twitter</span>
+            </Link>
+            <BottomGradient />
+          </button>
 
+          <button
+            className="relative group/btn flex space-x-2 items-center justify-start px-4 py-2 rounded-md font-medium bg-zinc-900 text-neutral-300 shadow-lg hover:shadow-xl transition-all"
+            type="button"
+          >
+            <Link
+              href="mailto:rohitrikame88@gmail.com" // Replace with your actual email
+              className="flex space-x-2"
+            >
+              {/* You can replace the icon below with any email icon you want */}
+              <IconMail className="h-5 w-5" />
+              <span>Email Me</span>
+            </Link>
+            <BottomGradient />
+          </button>
 
-        {/* GitHub Button */}
-        <button
-          className="relative group/btn flex space-x-2 items-center justify-start px-4 py-2 rounded-md font-medium bg-zinc-900 text-neutral-300 shadow-lg hover:shadow-xl transition-all"
-          type="button"
-        >
-          <Link href="https://github.com/elo369" target="_blank" rel="noopener noreferrer" className="flex space-x-2">
-            <IconBrandGithub className="h-5 w-5" />
-          <span>GitHub</span>
-          </Link>
-          <BottomGradient />
-        </button>
+          {/* GitHub Button */}
+          <button
+            className="relative group/btn flex space-x-2 items-center justify-start px-4 py-2 rounded-md font-medium bg-zinc-900 text-neutral-300 shadow-lg hover:shadow-xl transition-all"
+            type="button"
+          >
+            <Link
+              href="https://github.com/elo369"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex space-x-2"
+            >
+              <IconBrandGithub className="h-5 w-5" />
+              <span>GitHub</span>
+            </Link>
+            <BottomGradient />
+          </button>
 
-        {/* LinkedIn Button */}
-        <button
-          className="relative group/btn flex space-x-2 items-center justify-start px-4 py-2 rounded-md font-medium bg-zinc-900 text-neutral-300 shadow-lg hover:shadow-xl transition-all"
-          type="button"
-        >
-          <Link href="https://www.linkedin.com/in/rohit-rikame-6423a0288/" target="_blank" rel="noopener noreferrer" className="flex space-x-2">
-            <IconBrandLinkedin className="h-5 w-5" />
-          <span>LinkedIn</span>
-          </Link>
-          <BottomGradient />
-        </button>
-
-      </div>
-
+          {/* LinkedIn Button */}
+          <button
+            className="relative group/btn flex space-x-2 items-center justify-start px-4 py-2 rounded-md font-medium bg-zinc-900 text-neutral-300 shadow-lg hover:shadow-xl transition-all"
+            type="button"
+          >
+            <Link
+              href="https://www.linkedin.com/in/rohit-rikame-6423a0288/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex space-x-2"
+            >
+              <IconBrandLinkedin className="h-5 w-5" />
+              <span>LinkedIn</span>
+            </Link>
+            <BottomGradient />
+          </button>
+        </div>
       </section>
-
-     
     </div>
   );
-};
+}
 
 const BottomGradient = () => {
   return (
--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import { SparklesCore } from "./ui/sparkles";
 
 interface tech {
-  image:string,
-  name:string
+  image: string;
+  name: string;
 }
 const TechStack = () => {
-  const techStacks:tech[] = [
+  const techStacks: tech[] = [
     {
       image: "/html.png",
       name: "HTML",
@@ -49,8 +49,13 @@ const TechStack = () => {
       name: "Mongodb",
     },
   ];
+
+ 
   return (
-    <div id="tech" className="min-h-screen sm:min-h-[70vh] relative py-20 sm:top:10 ">
+    <div
+      id="tech"
+      className="min-h-screen sm:min-h-[70vh] relative py-20 sm:top:10 "
+    >
       <div>
         <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-4xl md:text-6xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight bottom-5 sm:text-5xl md:top-1 bg-black">
           Tech Stack
@@ -76,24 +81,27 @@ const TechStack = () => {
           <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
         </div>
       </div>
-        <div className="flex flex-wrap flex-row lg:w-full md:w-[80vw] sm:w-[90vw] sm:m-auto md:m-auto h-[30vh] bg-black gap-7 lg:px-27  relative md:bottom-0  b justify-center items-center min-h-70 bottom-35">
-          {techStacks.map((stacks) => (
-            <div
-              key={stacks.name}
-              className="border border-black lg:p-7 md:p-3 sm:p-4 flex flex-col justify-center items-center text-white bg-[#303030] rounded-md lg:w-50 md:w-25 sm:w-18 gap-1 w-30 px-10 py-2"
-            >
-              <Image
-                width={80}
-                height={50}
-                src={stacks.image}
-                alt="image"
-                className="w-17 "
-              />
-              <p className="sm:text-sm md:text-md text-[10px] bg-gradient-to-b from-neutral-500 to-white bg-clip-text text-transparent">{stacks.name}</p>
-            </div>
-          ))}
-        </div>
+      <div  
+     className="flex flex-wrap flex-row lg:w-full md:w-[80vw] sm:w-[90vw] sm:m-auto md:m-auto h-[30vh] bg-black gap-7 lg:px-27  relative md:bottom-0  b justify-center items-center min-h-70 bottom-35">
+        {techStacks.map((stacks) => (
+          <div
+            key={stacks.name}
+            className="border border-black lg:p-7 md:p-3 sm:p-4 flex flex-col justify-center items-center text-white bg-[#303030] rounded-md lg:w-50 md:w-25 sm:w-18 gap-1 w-30 px-10 py-2"
+          >
+            <Image
+              width={80}
+              height={50}
+              src={stacks.image}
+              alt="image"
+              className="w-17 "
+            />
+            <p className="sm:text-sm md:text-md text-[10px] bg-gradient-to-b from-neutral-500 to-white bg-clip-text text-transparent">
+              {stacks.name}
+            </p>
+          </div>
+        ))}
       </div>
+    </div>
   );
 };
 
